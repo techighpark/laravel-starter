5283e0de697841a2cba086aa3271f11b5c45fda7
techigh <techigh.b@gmail.com>
2024-01-18 07:03:21 +0900

    feat: sanctum api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/test', function (Request $request){
+Route::get('/test', function (Request $request) {
 
     $posts = Post::query()->get();
 
@@ -23,11 +26,52 @@ Route::get('/test', function (Request $request){
 //    \App\Models\User::factory()->count(12)->create();
 //    return $user;
 });
+
+Route::post('/sanctum/token', function (Request $request) {
+    
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
+
+    $credentials = $request->only([
+        'email',
+        'password',
+        'device_name',
+    ]);
+
+    Log::debug(json_encode($credentials));
+
+    /** @var User $user */
+    $user = User::query()->where('email', $credentials['email'])->first();
+
+    if (!$user || !Hash::check($credentials['password'], $user->getAttribute('password'))) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    Log::debug('1' . $user->getAuthPassword());
+    Log::debug('2' . $user->getAttribute('password'));
+
+
+    return $user->createToken($credentials['device_name'])->plainTextToken;
+
+});
+
+Route::post('/tokens/create', function (Request $request) {
+
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
 Route::middleware('auth:sanctum')
-    ->group(function (){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    ->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
 
 //    Route::
-});
+    });
